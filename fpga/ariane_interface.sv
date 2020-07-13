6721b793200981780d99069affdbb7899aa33e91
Ruige Lee <295054118@qq.com>
2020-07-13 17:07:32 +0800

    connection complete

--- fpga/ariane_interface.sv
+++ fpga/ariane_interface.sv
@@ -20,53 +20,53 @@ module ariane_interface (
 	// memory side, AXI Master
 
 
-		input wire  M_AXI_ACLK,
-		input wire  M_AXI_ARESETN,
+	// input wire  M_AXI_ACLK,
+	// input wire  M_AXI_ARESETN,
 
 
 
-			output wire [3 : 0] M_AXI_AWID,
-			output wire [63 : 0] M_AXI_AWADDR,
-			output wire [7 : 0] M_AXI_AWLEN,
-			output wire [2 : 0] M_AXI_AWSIZE,
-			output wire [1 : 0] M_AXI_AWBURST,
-			output wire  M_AXI_AWLOCK,
-			output wire [3 : 0] M_AXI_AWCACHE,
-			output wire [2 : 0] M_AXI_AWPROT,
-			output wire [3 : 0] M_AXI_AWQOS,
-		output wire [C_M_AXI_AWUSER_WIDTH-1 : 0] M_AXI_AWUSER,
-			output wire  M_AXI_AWVALID,
-			input wire  M_AXI_AWREADY,
-			output wire [63 : 0] M_AXI_WDATA,
-			output wire [7 : 0] M_AXI_WSTRB,
-			output wire  M_AXI_WLAST,
-		output wire [C_M_AXI_WUSER_WIDTH-1 : 0] M_AXI_WUSER,
-			output wire  M_AXI_WVALID,
-			input wire  M_AXI_WREADY,
-			input wire [3 : 0] M_AXI_BID,
-			input wire [1 : 0] M_AXI_BRESP,
-		input wire [C_M_AXI_BUSER_WIDTH-1 : 0] M_AXI_BUSER,
-			input wire  M_AXI_BVALID,
-			output wire  M_AXI_BREADY,
-			output wire [3 : 0] M_AXI_ARID,
-			output wire [63 : 0] M_AXI_ARADDR,
-			output wire [7 : 0] M_AXI_ARLEN,
-			output wire [2 : 0] M_AXI_ARSIZE,
-			output wire [1 : 0] M_AXI_ARBURST,
-			output wire  M_AXI_ARLOCK,
-			output wire [3 : 0] M_AXI_ARCACHE,
-			output wire [2 : 0] M_AXI_ARPROT,
-			output wire [3 : 0] M_AXI_ARQOS,
-		output wire [C_M_AXI_ARUSER_WIDTH-1 : 0] M_AXI_ARUSER,
-			output wire  M_AXI_ARVALID,
-			input wire  M_AXI_ARREADY,
-			input wire [3 : 0] M_AXI_RID,
-			input wire [63 : 0] M_AXI_RDATA,
-			input wire [1 : 0] M_AXI_RRESP,
-			input wire  M_AXI_RLAST,
-		input wire [C_M_AXI_RUSER_WIDTH-1 : 0] M_AXI_RUSER,
-			input wire  M_AXI_RVALID,
-			output wire  M_AXI_RREADY
+	output wire [3 : 0] M_AXI_AWID,
+	output wire [63 : 0] M_AXI_AWADDR,
+	output wire [7 : 0] M_AXI_AWLEN,
+	output wire [2 : 0] M_AXI_AWSIZE,
+	output wire [1 : 0] M_AXI_AWBURST,
+	output wire  M_AXI_AWLOCK,
+	output wire [3 : 0] M_AXI_AWCACHE,
+	output wire [2 : 0] M_AXI_AWPROT,
+	output wire [3 : 0] M_AXI_AWQOS,
+// output wire [C_M_AXI_AWUSER_WIDTH-1 : 0] M_AXI_AWUSER,
+	output wire  M_AXI_AWVALID,
+	input wire  M_AXI_AWREADY,
+	output wire [63 : 0] M_AXI_WDATA,
+	output wire [7 : 0] M_AXI_WSTRB,
+	output wire  M_AXI_WLAST,
+// output wire [C_M_AXI_WUSER_WIDTH-1 : 0] M_AXI_WUSER,
+	output wire  M_AXI_WVALID,
+	input wire  M_AXI_WREADY,
+	input wire [3 : 0] M_AXI_BID,
+	input wire [1 : 0] M_AXI_BRESP,
+// input wire [C_M_AXI_BUSER_WIDTH-1 : 0] M_AXI_BUSER,
+	input wire  M_AXI_BVALID,
+	output wire  M_AXI_BREADY,
+	output wire [3 : 0] M_AXI_ARID,
+	output wire [63 : 0] M_AXI_ARADDR,
+	output wire [7 : 0] M_AXI_ARLEN,
+	output wire [2 : 0] M_AXI_ARSIZE,
+	output wire [1 : 0] M_AXI_ARBURST,
+	output wire  M_AXI_ARLOCK,
+	output wire [3 : 0] M_AXI_ARCACHE,
+	output wire [2 : 0] M_AXI_ARPROT,
+	output wire [3 : 0] M_AXI_ARQOS,
+// output wire [C_M_AXI_ARUSER_WIDTH-1 : 0] M_AXI_ARUSER,
+	output wire  M_AXI_ARVALID,
+	input wire  M_AXI_ARREADY,
+	input wire [3 : 0] M_AXI_RID,
+	input wire [63 : 0] M_AXI_RDATA,
+	input wire [1 : 0] M_AXI_RRESP,
+	input wire  M_AXI_RLAST,
+// input wire [C_M_AXI_RUSER_WIDTH-1 : 0] M_AXI_RUSER,
+	input wire  M_AXI_RVALID,
+	output wire  M_AXI_RREADY
 
 );
 
@@ -79,123 +79,123 @@ module ariane_interface (
 
 
 
-	// ariane_axi::req_t             axi_req_o;
+	ariane_axi::req_t             axi_req_o;
 
 
         // aw_chan_t axi_req_o_aw;
 
-        logic [3:0]              axi_req_o_aw_id;
-        assign M_AXI_AWID = axi_req_o_aw_id;
+        // logic [3:0]              axi_req_o_aw_id;
+        assign M_AXI_AWID = axi_req_o.aw.id;
 
-        logic [63:0]            axi_req_o_aw_addr;
-        assign M_AXI_AWADDR = axi_req_o_aw_addr;
+        // logic [63:0]            axi_req_o_aw_addr;
+        assign M_AXI_AWADDR = axi_req_o.aw.addr;
 
-        logic [7:0]    axi_req_o_aw_len;
-        assign M_AXI_AWLEN = axi_req_o_aw_len;
+        // logic [7:0]    axi_req_o_aw_len;
+        assign M_AXI_AWLEN = axi_req_o.aw.len;
 
-        logic [2:0]   axi_req_o_aw_size;
-        assign M_AXI_AWSIZE = axi_req_o_aw_size;
+        // logic [2:0]   axi_req_o_aw_size;
+        assign M_AXI_AWSIZE = axi_req_o.aw.size;
 
-        logic [1:0]  axi_req_o_aw_burst;
-        assign M_AXI_AWBURST = axi_req_o_aw_burst;
+        // logic [1:0]  axi_req_o_aw_burst;
+        assign M_AXI_AWBURST = axi_req_o.aw.burst;
 
-        logic axi_req_o_aw_lock;
-        assign M_AXI_AWLOCK = axi_req_o_aw_lock;
+        // logic axi_req_o_aw_lock;
+        assign M_AXI_AWLOCK = axi_req_o.aw.lock;
 
-        logic [3:0]  axi_req_o_aw_cache;
-        assign M_AXI_AWCACHE = axi_req_o_aw_cache;
+        // logic [3:0]  axi_req_o_aw_cache;
+        assign M_AXI_AWCACHE = axi_req_o.aw.cache;
 
-        logic [2:0]   axi_req_o_aw_prot;
-        assign M_AXI_AWPROT = axi_req_o_aw_prot;
+        // logic [2:0]   axi_req_o_aw_prot;
+        assign M_AXI_AWPROT = axi_req_o.aw.prot;
 
-        logic [3:0]    axi_req_o_aw_qos;
-        assign M_AXI_AWQOS = axi_req_o_aw_qos;
+        // logic [3:0]    axi_req_o_aw_qos;
+        assign M_AXI_AWQOS = axi_req_o.aw.qos;
 
-        logic [3:0] axi_req_o_aw_region;
+        // logic [3:0] axi_req_o_aw_region;
 
-        logic [5:0]   axi_req_o_aw_atop;
+        // logic [5:0]   axi_req_o_aw_atop;
 
-        logic axi_req_o_aw_valid;
-        assign M_AXI_AWVALID = axi_req_o_aw_valid;
+        // logic axi_req_o_aw_valid;
+        assign M_AXI_AWVALID = axi_req_o.aw.valid;
 
         // w_chan_t  axi_req_o_w;
-        logic [63:0] axi_req_o_w_data;
-        assign M_AXI_WDATA = axi_req_o_w_data;
+        // logic [63:0] axi_req_o_w_data;
+        assign M_AXI_WDATA = axi_req_o.w.data;
 
-        logic [7:0] axi_req_o_w_strb;
-        assign M_AXI_WSTRB = axi_req_o_w_strb;
+        // logic [7:0] axi_req_o_w_strb;
+        assign M_AXI_WSTRB = axi_req_o.w.strb;
 
-        logic axi_req_o_w_last;
-        assign M_AXI_WLAST = axi_req_o_w_last;
+        // logic axi_req_o_w_last;
+        assign M_AXI_WLAST = axi_req_o.w.last;
 
-        logic axi_req_o_w_valid;
-        assign M_AXI_WVALID = axi_req_o_w_valid;
+        // logic axi_req_o_w_valid;
+        assign M_AXI_WVALID = axi_req_o.w.valid;
 
-        logic axi_req_o_b_ready;
-        assign M_AXI_BREADY = axi_req_o_b_ready;
+        // logic axi_req_o_b_ready;
+        assign M_AXI_BREADY = axi_req_o.b.ready;
         // ar_chan_t axi_req_o_ar;
-        logic [3:0]             axi_req_o_ar_id;
-        assign M_AXI_ARID = axi_req_o_ar_id;
+        // logic [3:0]             axi_req_o_ar_id;
+        assign M_AXI_ARID = axi_req_o.ar.id;
 
-        logic [63:0]            axi_req_o_ar_addr;
-        assign M_AXI_ARADDR = axi_req_o_ar_addr;
+        // logic [63:0]            axi_req_o_ar_addr;
+        assign M_AXI_ARADDR = axi_req_o.ar.addr;
 
-        logic [7:0]    axi_req_o_ar_len;
-        assign M_AXI_ARLEN = axi_req_o_ar_len;
+        // logic [7:0]    axi_req_o_ar_len;
+        assign M_AXI_ARLEN = axi_req_o.ar.len;
 
-        logic [2:0]   axi_req_o_ar_size;
-        assign M_AXI_ARSIZE = axi_req_o_ar_size;
+        // logic [2:0]   axi_req_o_ar_size;
+        assign M_AXI_ARSIZE = axi_req_o.ar.size;
 
-        logic [1:0]  axi_req_o_ar_burst;
-        assign M_AXI_ARBURST = axi_req_o_ar_burst;
+        // logic [1:0]  axi_req_o_ar_burst;
+        assign M_AXI_ARBURST = axi_req_o.ar.burst;
 
-        logic             axi_req_o_ar_lock;
-        assign M_AXI_ARLOCK = axi_req_o_ar_lock;
+        // logic             axi_req_o_ar_lock;
+        assign M_AXI_ARLOCK = axi_req_o.ar.lock;
 
-        logic [3:0]  axi_req_o_ar_cache;
-        assign M_AXI_ARCACHE = axi_req_o_ar_cache;
+        // logic [3:0]  axi_req_o_ar_cache;
+        assign M_AXI_ARCACHE = axi_req_o.ar.cache;
 
-        logic [2:0]   axi_req_o_ar_prot;
-        assign M_AXI_ARPROT = axi_req_o_ar_prot;
+        // logic [2:0]   axi_req_o_ar_prot;
+        assign M_AXI_ARPROT = axi_req_o.ar.prot;
 
-        logic [3:0]    axi_req_o_ar_qos;
-        assign M_AXI_ARQOS = axi_req_o_ar_qos;
+        // logic [3:0]    axi_req_o_ar_qos;
+        assign M_AXI_ARQOS = axi_req_o.ar.qos;
 
-        logic [3:0] axi_req_o_ar_region;
+        // logic [3:0] axi_req_o_ar_region;
 
-        logic     axi_req_o_ar_valid;
-        assign M_AXI_ARVALID = axi_req_o_ar_valid;
+        // logic     axi_req_o_ar_valid;
+        assign M_AXI_ARVALID = axi_req_o.ar.valid;
 
-        logic     axi_req_o_r_ready;
-        assign M_AXI_RREADY = axi_req_o_r_ready;
-
-
+        // logic     axi_req_o_r_ready;
+        assign M_AXI_RREADY = axi_req_o.r.ready;
 
 
 
 
 
-	// ariane_axi::resp_t            axi_resp_i;
-			  logic     axi_resp_i_aw_ready = M_AXI_AWREADY;
-        logic     axi_resp_i_ar_ready = M_AXI_ARREADY;
-        logic     axi_resp_i_w_ready = M_AXI_WREADY;
-        logic     axi_resp_i_b_valid = M_AXI_RVALID;
+
+
+		ariane_axi::resp_t            axi_resp_i;
+			  assign axi_resp_i.aw.ready = M_AXI_AWREADY;
+        assign axi_resp_i.ar.ready = M_AXI_ARREADY;
+        assign axi_resp_i.w.ready = M_AXI_WREADY;
+        assign axi_resp_i.b.valid = M_AXI_RVALID;
         // b_chan_t  axi_resp_i_b;
-        logic [3:0]            axi_resp_i_b_id = M_AXI_BID;
-        logic [1:0] axi_resp_i_b_resp = M_AXI_BRESP;
+        assign axi_resp_i.b.id = M_AXI_BID;
+        assign axi_resp_i.b.resp = M_AXI_BRESP;
 
-        logic     axi_resp_i_r_valid = M_AXI_BVALID;
+        assign axi_resp_i.r.valid = M_AXI_BVALID;
         // r_chan_t  axi_resp_i_r;
-        logic [3:0]            axi_resp_i_r_id = M_AXI_RID;
-        logic [63:0]          axi_resp_i_r_data = M_AXI_RDATA;
-        logic [1:0] axi_resp_i_r_resp = M_AXI_RRESP;
-        logic axi_resp_i_r_last = M_AXI_RLAST;
+        assign axi_resp_i.r.id = M_AXI_RID;
+        assign axi_resp_i.r.data = M_AXI_RDATA;
+        assign axi_resp_i.r.resp = M_AXI_RRESP;
+        assign axi_resp_i.r.last = M_AXI_RLAST;
 
 
 
 
 ariane i_ariane #(
-	parameter ariane_pkg::ariane_cfg_t ArianeCfg     = ariane_pkg::ArianeDefaultConfig
+	.ArianeCfg(ariane_pkg::ArianeDefaultConfig)
 ) (
 	.clk_i(clk_i),
 	.rst_ni(rst_ni),
--- fpga/ariane_wrap.v
+++ fpga/ariane_wrap.v
@@ -1,47 +1,117 @@
 
-
+		
 
 
 module ariane_wrap (
-		input S_AXI_ACLK,
-		input S_AXI_ARESETN,
-		input [C_S_AXI_ADDR_WIDTH-1 : 0] S_AXI_AWADDR,
-		input [2 : 0] S_AXI_AWPROT,
-		input S_AXI_AWVALID,
-		output S_AXI_AWREADY,
-		input [C_S_AXI_DATA_WIDTH-1 : 0] S_AXI_WDATA,
-		input [(C_S_AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
-		input S_AXI_WVALID,
-		output S_AXI_WREADY,
-		output [1 : 0] S_AXI_BRESP,
-		output S_AXI_BVALID,
-		input S_AXI_BREADY,
-		input [C_S_AXI_ADDR_WIDTH-1 : 0] S_AXI_ARADDR,
-		input [2 : 0] S_AXI_ARPROT,
-		input S_AXI_ARVALID,
-		output S_AXI_ARREADY,
-		output [C_S_AXI_DATA_WIDTH-1 : 0] S_AXI_RDATA,
-		output [1 : 0] S_AXI_RRESP,
-		output S_AXI_RVALID,
-		input S_AXI_RREADY
+	input  wire                         clk_i,
+	input  wire                         rst_ni,
+	// Core ID, Cluster ID and boot address are considered more or less static
+	input  wire [63:0]                  boot_addr_i,  // reset boot address
+	input  wire [63:0]                  hart_id_i,    // hart id in a multicore environment (reflected in a CSR)
+
+	// Interrupt inputs
+	input  wire [1:0]                   irq_i,        // level sensitive IR lines, mip & sip (async)
+	input  wire                         ipi_i,        // inter-processor interrupts (async)
+	// Timer facilities
+	input  wire                         time_irq_i,   // timer interrupt in (async)
+	input  wire                         debug_req_i,  // debug request (async)
+		// input wire  M_AXI_ACLK,
+		// input wire  M_AXI_ARESETN,
+		output wire [C_M_AXI_ID_WIDTH-1 : 0] M_AXI_AWID,
+		output wire [C_M_AXI_ADDR_WIDTH-1 : 0] M_AXI_AWADDR,
+		output wire [7 : 0] M_AXI_AWLEN,
+		output wire [2 : 0] M_AXI_AWSIZE,
+		output wire [1 : 0] M_AXI_AWBURST,
+		output wire  M_AXI_AWLOCK,
+		output wire [3 : 0] M_AXI_AWCACHE,
+		output wire [2 : 0] M_AXI_AWPROT,
+		output wire [3 : 0] M_AXI_AWQOS,
+		output wire [C_M_AXI_AWUSER_WIDTH-1 : 0] M_AXI_AWUSER,
+		output wire  M_AXI_AWVALID,
+		input wire  M_AXI_AWREADY,
+		output wire [C_M_AXI_DATA_WIDTH-1 : 0] M_AXI_WDATA,
+		output wire [C_M_AXI_DATA_WIDTH/8-1 : 0] M_AXI_WSTRB,
+		output wire  M_AXI_WLAST,
+		output wire [C_M_AXI_WUSER_WIDTH-1 : 0] M_AXI_WUSER,
+		output wire  M_AXI_WVALID,
+		input wire  M_AXI_WREADY,
+		input wire [C_M_AXI_ID_WIDTH-1 : 0] M_AXI_BID,
+		input wire [1 : 0] M_AXI_BRESP,
+		input wire [C_M_AXI_BUSER_WIDTH-1 : 0] M_AXI_BUSER,
+		input wire  M_AXI_BVALID,
+		output wire  M_AXI_BREADY,
+		output wire [C_M_AXI_ID_WIDTH-1 : 0] M_AXI_ARID,
+		output wire [C_M_AXI_ADDR_WIDTH-1 : 0] M_AXI_ARADDR,
+		output wire [7 : 0] M_AXI_ARLEN,
+		output wire [2 : 0] M_AXI_ARSIZE,
+		output wire [1 : 0] M_AXI_ARBURST,
+		output wire  M_AXI_ARLOCK,
+		output wire [3 : 0] M_AXI_ARCACHE,
+		output wire [2 : 0] M_AXI_ARPROT,
+		output wire [3 : 0] M_AXI_ARQOS,
+		output wire [C_M_AXI_ARUSER_WIDTH-1 : 0] M_AXI_ARUSER,
+		output wire  M_AXI_ARVALID,
+		input wire  M_AXI_ARREADY,
+		input wire [C_M_AXI_ID_WIDTH-1 : 0] M_AXI_RID,
+		input wire [C_M_AXI_DATA_WIDTH-1 : 0] M_AXI_RDATA,
+		input wire [1 : 0] M_AXI_RRESP,
+		input wire  M_AXI_RLAST,
+		input wire [C_M_AXI_RUSER_WIDTH-1 : 0] M_AXI_RUSER,
+		input wire  M_AXI_RVALID,
+		output wire  M_AXI_RREADY
+
 );
 
-typedef struct packed {
-      id_t     a;
-      addr_t   b;
-  } outType;
 
-outType out;
-assign out.a = out_a;
-assign out.b = out_b;
+module ariane_interface (
+	.clk_i(clk_i),
+	.rst_ni(rst_ni),
+	.boot_addr_i(boot_addr_i),
+	.hart_id_i(hart_id_i),
+	.irq_i(irq_i),
+	.ipi_i(ipi_i),
+	.time_irq_i(time_irq_i),
+	.debug_req_i(debug_req_i),
 
-test i_test(
-	.clk(clk),
-	.clk_en(clk_en),
-	.rst_n(rst_n),
-	.out(out)
+	.M_AXI_AWID(M_AXI_AWID),
+	.M_AXI_AWADDR(M_AXI_AWADDR),
+	.M_AXI_AWLEN(M_AXI_AWLEN),
+	.M_AXI_AWSIZE(M_AXI_AWSIZE),
+	.M_AXI_AWBURST(M_AXI_AWBURST),
+	.M_AXI_AWLOCK(M_AXI_AWLOCK),
+	.M_AXI_AWCACHE(M_AXI_AWCACHE),
+	.M_AXI_AWPROT(M_AXI_AWPROT),
+	.M_AXI_AWQOS(M_AXI_AWQOS),
+	.M_AXI_AWVALID(M_AXI_AWVALID),
+	.M_AXI_AWREADY(M_AXI_AWREADY),
+	.M_AXI_WDATA(M_AXI_WDATA),
+	.M_AXI_WSTRB(M_AXI_WSTRB),
+	.M_AXI_WLAST(M_AXI_WLAST),
+	.M_AXI_WVALID(M_AXI_WVALID),
+	.M_AXI_WREADY(M_AXI_WREADY),
+	.M_AXI_BID(M_AXI_BID),
+	.M_AXI_BRESP(M_AXI_BRESP),
+	.M_AXI_BVALID(M_AXI_BVALID),
+	.M_AXI_BREADY(M_AXI_BREADY),
+	.M_AXI_ARID(M_AXI_ARID),
+	.M_AXI_ARADDR(M_AXI_ARADDR),
+	.M_AXI_ARLEN(M_AXI_ARLEN),
+	.M_AXI_ARSIZE(M_AXI_ARSIZE),
+	.M_AXI_ARBURST(M_AXI_ARBURST),
+	.M_AXI_ARLOCK(M_AXI_ARLOCK),
+	.M_AXI_ARCACHE(M_AXI_ARCACHE),
+	.M_AXI_ARPROT(M_AXI_ARPROT),
+	.M_AXI_ARQOS(M_AXI_ARQOS),
+	.M_AXI_ARVALID(M_AXI_ARVALID),
+	.M_AXI_ARREADY(M_AXI_ARREADY),
+	.M_AXI_RID(M_AXI_RID),
+	.M_AXI_RDATA(M_AXI_RDATA),
+	.M_AXI_RRESP(M_AXI_RRESP),
+	.M_AXI_RLAST(M_AXI_RLAST),
+	.M_AXI_RVALID(M_AXI_RVALID),
+	.M_AXI_RREADY(M_AXI_RREADY)
+
 );
-
 
 
 
